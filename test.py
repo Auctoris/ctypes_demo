56b64ef1bb3047d926b45fb08ebc630eeb2b5d4c
Auctoris <ajp@auctoris.co.uk>
2017-04-28 14:17:05 +0100

    Add improved test.py

--- test.py
+++ test.py
@@ -1,8 +1,24 @@
 from foo import Foo
 
+# We'll create a Foo object with a value of 5...
 f=Foo(5)
+
+# Calling Foo.bar() will print a message including the value...
+print ('f=Foo(5)')
+print ('\t'),
 f.bar()
+print
+
+# Now we'll use foobar to add a value to that stored in our Foo object, f
+print ("print (f.foobar(7)) = "),
 print (f.foobar(7))
+print
+
+# Now we'll do the same thing - but this time demonstrate that it's a normal
+# Python integer...
 
 x = f.foobar(2)
-print (x)
+print ("Type of x; where x = f.foobar(2)")
+print ("\t"),
+print (type(x))
+print 
