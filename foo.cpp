8dafcf821250936b4e3b89761d0be425021c3070
Auctoris <ajp@auctoris.co.uk>
2017-04-28 12:26:24 +0100

    Add example code to foo.cpp

---
+++ foo.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// A simple class with a constuctor and one method...
+
+class Foo
+{
+    public:
+        Foo(int);
+        void bar();
+    private:
+        int val;
+};
+
+Foo::Foo(int n)
+{
+    val = n;
+}
+
+void Foo::bar()
+{
+    std::cout << "Value is " << val << std::endl;
+}
+
+// Define C functions for the C++ class - as ctypes can only talk to C...
+
+extern "C"
+{
+    Foo* Foo_new(int n){return new Foo(n);}
+    void Foo_bar(Foo* foo) {foo->bar();}
+}
+
